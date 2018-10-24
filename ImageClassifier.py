57e41720ee2d7379ea7dfad9b3d2e00dc9c5d6a5
Pranav Vempati <pranav.k.vempati@gmail.com>
2018-10-23 23:44:56 -0700

    Update ImageClassifier.py

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -7,20 +7,11 @@ import torch.nn as nn
 import torch.nn.functional as F 
 import torch.optim as optim
 
-
-
-
-
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
-
 training_set = torchvision.datasets.CIFAR10(root = './data', train = True, download = True, transform = transform)
-
 trainloader = torch.utils.data.DataLoader(training_set, batch_size = 4, shuffle = True, num_workers = 2)
-
 testing_set = torchvision.datasets.CIFAR10(root = './data', train = False, download = True, transform = transform)
-
 testloader = torch.utils.data.DataLoader(testing_set, batch_size = 4, shuffle = False, num_workers = 2) 
-
 classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck') # Predefined CIFAR 10 classes
 
 
@@ -52,31 +43,22 @@ for epoch in range(5):
 	running_loss = 0.0
 	for index, data in enumerate(trainloader, 0):
 		inputs, labels = data
-
 		optimizer.zero_grad() # Zero parametric gradient buffers
-
 		outputs = network(inputs)
 		loss = criterion(outputs, labels)
 		loss.backward()
 		optimizer.step()
-
 		running_loss+=loss.item()
-
 		if index % 2000 == 1999:
 			 print('[%d, %5d] loss: %.3f' %(epoch + 1, index + 1, running_loss / 2000)) # Log successive running loss to stdout at 2,000 minibatch intervals
 			 running_loss = 0.0
 
 print("Finished training")
 
-
 dataiter = iter(testloader)
-
 images, labels = dataiter.next()
-
 outputs = network(images)
-
 _, predicted = torch.max(outputs, 1) # Retrieve the latter value, namely, the column indices of the supremum along each row
-
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 
 
